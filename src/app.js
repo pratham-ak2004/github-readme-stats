e9365a4788f23a8c7280eaf00f883c21e3166389
Pratham A Kadekar <pratham20442@gmail.com>
2026-01-07 16:30:18 +0000

    [refactor] file location

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import "dotenv/config";
-import statsCard from "./api/index.js";
-import repoCard from "./api/pin.js";
-import langCard from "./api/top-langs.js";
-import wakatimeCard from "./api/wakatime.js";
-import gistCard from "./api/gist.js";
+import statsCard from "../api/index.js";
+import repoCard from "../api/pin.js";
+import langCard from "../api/top-langs.js";
+import wakatimeCard from "../api/wakatime.js";
+import gistCard from "../api/gist.js";
 import express from "express";
 
 const app = express();
